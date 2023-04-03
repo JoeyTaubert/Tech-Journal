5706d81bba1216b44ff1b46084e91c339df57153
JoeyTaubert <joseph.taubert@mymail.champlain.edu>
2023-04-03 12:08:51 -0400

    Some PS scripts

---
+++ PowerShell/newPasswordlessUser.ps1
@@ -0,0 +1,5 @@
+﻿$User = Read-Host "Enter username for passwordless local account"
+
+New-LocalUser -Name $User -NoPassword 
+
+Set-Localuser -Name $User -PasswordNeverExpires:$true
---
+++ PowerShell/newUser.ps1
@@ -0,0 +1,11 @@
+﻿#Set username
+$User = Read-Host "Enter the username"
+
+#Set password
+$Password = Read-Host "Enter the password" -AsSecureString
+
+#Create user with specified creds
+New-LocalUser -Name $User -Password $Password
+
+#Add user to admin group
+Add-LocalGroupMember -Group "Administrators" -Member $User
