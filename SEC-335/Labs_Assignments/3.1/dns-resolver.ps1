e423e8e3ed824fe29eb1470e1e9873eff0d45d8f
JoeyTaubert <joey.taubert@comcast.net>
2023-09-17 14:27:30 -0400

    Add PS script for L3.1

---
+++ SEC-335/Labs_Assignments/3.1/dns-resolver.ps1
@@ -0,0 +1,14 @@
+﻿# Made by Joey Taubert. Credit to ChatGPT for assisting me with checking if the value of a varaible is null, and for providing me with the -ExpandProperty flag.
+
+# Grabs the second and third input provided at the command line
+param($network, $server)
+
+# This for loop iterates through .1-.254 IPs, assuming that the first three octects are provided
+for ($ip = 1; $ip -le 254; $ip++){
+    $ipv4 = $network + "." + $ip
+    $name = Resolve-DnsName -DnsOnly $ipv4 -Server $server -ErrorAction Ignore | select  -ExpandProperty NameHost 
+        # Only print if a FQDN was found
+        if ($name -ne $null){
+        Write-Host "$ipv4 $name"
+        } 
+}
